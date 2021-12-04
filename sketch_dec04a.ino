66e25903a7ea367c1ee7c20ddbd2461d8c56fd08
XingtaiHuang <56592377+XingtaiHuang@users.noreply.github.com>
2021-12-04 14:25:27 -0800

    Add files via upload

--- sketch_dec04a.ino
+++ sketch_dec04a.ino
@@ -2,6 +2,7 @@ int redPin = 11;
 int greenPin = 9;
 int bluePin = 10;
 int MoPin = 6; // vibrator Grove connected to digital pin 4
+int thres = 100;  //亮度
 
 void setup() {
   // put your setup code here, to run once:
@@ -17,8 +18,21 @@ void loop() {
       //R:0-255 G:0-255 B:0-255
       //colorRGB(random(0,255),random(0,255),random(0,255));  
   //delay(1000);
-      baseOn(1000,5);
-      baseOff(1000,5);
+//      未激活状态
+//      baseOn(1000,5); 
+//      baseOff(1000,5);
+
+//      trigger1状态
+//      trigger1On(1000,5);
+//      trigger1Off(1000,5);
+//      
+//      trigger2状态
+//      trigger2On(1000,5);
+//      trigger2Off(1000,5);
+        
+//      同时trigger状态        
+        bothOn(1000,5);
+        bothOff(1000,5);
 
 //      digitalWrite(MoPin, HIGH);
 //      delay(1000);
@@ -32,16 +46,16 @@ void loop() {
 //}
 
 void baseOn(unsigned int time,int increament){
-        for (byte value = 0 ; value < 50; value+=increament){ 
+        for (byte value = 0 ; value < thres; value+=increament){ 
             analogWrite(redPin, value);
             analogWrite(greenPin, value*0.4); 
             analogWrite(bluePin, value/4);  
-            delay(time/(255/5));
+            delay(100);
         } 
 }
 
 void baseOff(unsigned int time,int decreament){
-        for (byte value = 50; value >0; value-=decreament){ 
+        for (byte value = thres; value >0; value-=decreament){ 
             analogWrite(redPin, value);
             analogWrite(greenPin, value*0.4); 
             analogWrite(bluePin, value/4);  
@@ -49,21 +63,76 @@ void baseOff(unsigned int time,int decreament){
         }
 }
 
-
-void fadeOn(unsigned int time,int increament){
-        for (byte value = 0 ; value < 255; value+=increament){ 
-            analogWrite(redPin, constrain(value,0,255));
-            analogWrite(greenPin, constrain(value,0,255)); 
-            analogWrite(bluePin, constrain(value,0,255));  
+void trigger1On(unsigned int time,int increament){
+        for (byte value = 0 ; value < thres; value+=increament){ 
+            analogWrite(redPin, value);
+            //analogWrite(greenPin, value*0.4); 
+            //analogWrite(bluePin, value/4);  
             delay(100);
         } 
 }
 
-void fadeOff(unsigned int time,int decreament){
-        for (byte value = 255; value >0; value-=decreament){ 
-            analogWrite(redPin, constrain(value,0,255));
-            analogWrite(greenPin, constrain(value,0,255)); 
-            analogWrite(bluePin, constrain(value,0,255));  
-                delay(time/(255/5)); 
+void trigger1Off(unsigned int time,int decreament){
+        for (byte value = thres; value >0; value-=decreament){ 
+            analogWrite(redPin, value);
+            //analogWrite(greenPin, value*0.4); 
+            //analogWrite(bluePin, value/4);  
+            delay(100); 
         }
 }
+
+void trigger2On(unsigned int time,int increament){
+        for (byte value = 0 ; value < thres; value+=increament){ 
+            //analogWrite(redPin, value);
+            //analogWrite(greenPin, value*0.4); 
+            analogWrite(bluePin, value);  
+            delay(100);
+        } 
+}
+
+void trigger2Off(unsigned int time,int decreament){
+        for (byte value = thres; value >0; value-=decreament){ 
+//            analogWrite(redPin, value);
+//            analogWrite(greenPin, value*0.4); 
+            analogWrite(bluePin, value);  
+            delay(100); 
+        }
+}
+
+void bothOn(unsigned int time,int increament){
+        for (byte value = 0 ; value < thres; value+=increament){ 
+            analogWrite(redPin, value);
+            delay(100);
+            //analogWrite(greenPin, value*0.4); 
+            analogWrite(bluePin, value);  
+            delay(100);
+        } 
+}
+
+void bothOff(unsigned int time,int decreament){
+        for (byte value = thres; value >0; value-=decreament){ 
+            analogWrite(redPin, value);
+            delay(100);
+            //analogWrite(greenPin, value*0.4); 
+            analogWrite(bluePin, value);  
+            delay(100); 
+        }
+}
+
+//void fadeOn(unsigned int time,int increament){
+//        for (byte value = 0 ; value < 255; value+=increament){ 
+//            analogWrite(redPin, constrain(value,0,255));
+//            analogWrite(greenPin, constrain(value,0,255)); 
+//            analogWrite(bluePin, constrain(value,0,255));  
+//            delay(100);
+//        } 
+//}
+//
+//void fadeOff(unsigned int time,int decreament){
+//        for (byte value = 255; value >0; value-=decreament){ 
+//            analogWrite(redPin, constrain(value,0,255));
+//            analogWrite(greenPin, constrain(value,0,255)); 
+//            analogWrite(bluePin, constrain(value,0,255));  
+//                delay(time/(255/5)); 
+//        }
+//}
